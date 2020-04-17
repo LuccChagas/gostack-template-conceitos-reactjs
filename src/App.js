792ad9e5a66c0743680c20390c84237f529499bd
luccChagas <luccaa.chagas23@gmail.com>
2020-04-16 23:44:29 -0300

    DESAFIO FINAL - COM TESTES

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+const [repositories, setRepositories] = useState([]);
+
+useEffect(() => {
+  api.get("/repositories").then(response => {
+    setRepositories(response.data);
+  });
+}, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post("/repositories", {
+      title: 'Meu Repo',
+      url: "http://bitbucket.com/meurepo",
+      techs: ['Vue.js', 'JSF']
+    })
+
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    const newRepo = repositories.filter(
+      repo => repo.id !== id
+
+    )
+    setRepositories(newRepo);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repo => (
+          <li key={repo.id}>
+            {repo.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository(repo.id)}>
             Remover
           </button>
         </li>
+      ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
